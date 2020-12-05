8a5d78c6e16d4a211617d66f148792abe93db005
Ken Mayer <ken.mayer@meraki.net>
2020-12-04 17:11:06 -0800

    Refactoring, PasswordPolicy base class

--- lib/password_policy.rb
+++ lib/password_policy.rb
@@ -1,26 +1,21 @@
-class SledRentalPasswordPolicy
+class PasswordPolicy
   attr_reader :range, :character, :password
   def initialize(passwords_data)
-    range, character, @password = passwords_data.split(/\s+/)
-    @range = Range.new(*range.split("-").map(&:to_i))
-    @character = character.chop # ":"
+    _range, _character, @password = passwords_data.split(/\s+/)
+    @range = Range.new(*_range.split("-").map(&:to_i))
+    @character = _character.chop # ":"
   end
+end
 
+class SledRentalPasswordPolicy < PasswordPolicy
   def valid?
     char_counts = password.split("").group_by(&:itself)
     range === (char_counts.fetch(character, []).count)
   end
 end
 
-class TobogganPasswordPolicy
-  attr_reader :range, :character, :password
-  def initialize(passwords_data)
-    range, character, @password = passwords_data.split(/\s+/)
-    @range = range.split("-").map(&:to_i)
-    @character = character.chop # ":"
-  end
-
+class TobogganPasswordPolicy < PasswordPolicy
   def valid?
-    (password[range.first - 1] == character) ^ (password[range.last - 1] == character)
+    (password[range.min - 1] == character) ^ (password[range.max - 1] == character)
   end
 end
