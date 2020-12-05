a3c959edb28593ca79e2cad0feb2cc802d103ae1
Ken Mayer <ken.mayer@meraki.net>
2020-12-05 11:09:32 -0800

    Refactoring
    I kept gettings rows & columns mixed up... so, get rid of the
    "Primitive Obsession" anti-pattern

--- lib/toboggan_traversal.rb
+++ lib/toboggan_traversal.rb
@@ -1,29 +1,31 @@
+# I kept getting rows & cols mixed up.
+Position = Struct.new(:col, :row)
+
 class TobogganTraversal
   TREE = "#"
   OPEN = "."
 
-  attr_reader :forrest, :width, :start, :traversal
-  def initialize(data, traversal = [3,1]) # row/down, col/right
+  attr_reader :forrest, :width, :traversal
+  def initialize(data, traversal = [3,1]) # x/col/right, y/row/down
     @forrest = data.split("\n")
     @width = @forrest.first.length
-    @start = start
-    @traversal = traversal
+    @traversal = Position.new(*traversal)
   end
 
   def trees
     count = 0
-    position = [0,0]
+    position = Position.new(0,0)
     loop do
       position = traverse(position)
-      break count if forrest[position.first].nil?
-      count += 1 if forrest[position.first][position.last] == TREE
+      break count if forrest[position.row].nil?
+      count += 1 if forrest[position.row][position.col] == TREE
     end
   end
 
   def traverse(position)
-    row = position.first + traversal.last
-    col = (position.last + traversal.first) % width
+    row = position.row + traversal.row
+    col = (position.col + traversal.col) % width
 
-    [row, col]
+    Position.new(col,row)
   end
 end
