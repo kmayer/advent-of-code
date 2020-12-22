40e22fa91a586fad95f02ca820b52699ed172b51
Ken Mayer <ken.mayer@meraki.net>
2020-12-22 10:32:05 -0800

    Tweaked the watcher script.

--- watcher.sh
+++ watcher.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh -e
 
 # By specifying the replacement string, "-I{}" but not mentioning it, all output from fswatch is swallowed
-fswatch --one-per-batch --recursive --follow-links --latency=0.250 . | xargs -n1 -I{} bundle exec rspec $*
+# Sometimes the rspec process will get disconnected from the parent process and run "forever"
+# Setting the ulimit will stop the process. Unfortunately, silently
+fswatch --one-per-batch --recursive --follow-links --latency=0.250 ./spec | (ulimit -t 60; xargs -n1 -I{} bundle exec rspec $*)
