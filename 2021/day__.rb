06509d6669c91d1770f036a02a55661dbcda593a
Ken Mayer <ken.mayer@meraki.net>
2021-12-05 13:22:59 -0800

    Modify the template so there's less ceremony
    - Dynamically define the test class name based on the file name:
      dayNN.rb -> DayNN
    - Read the sample input from the __END__ of the file, assign it
      to a constant LINES so that it can't be modified.
    - Predefine a parser method
    - chmod +x

--- 2021/day__.rb
+++ 2021/day__.rb
@@ -2,19 +2,37 @@
 
 require "minitest/autorun"
 
-class Day__ < Minitest::Test
+Class.new(Minitest::Test) do
+  def self.name
+    File.basename(__FILE__, '.rb').capitalize
+  end
+
+  LINES = DATA.readlines.map(&:freeze).freeze
+
+  def parser(lines)
+    lines
+  end
+
   def setup
-    @sample_input = [
-    ]
-    # @input = File.read("inputs/day__.txt").lines
+    @sample_input = parser(LINES)
+    @input = begin 
+      parser(File.read("inputs/#{File.basename(__FILE__, '.rb')}.txt").lines)
+    rescue Errno::ENOENT
+      []
+    end
   end
 
   def test_part1a
+    puts @sample_input.inspect
   end
+
   def test_part1b
   end
+
   def test_part2a
   end
+
   def test_part_2b
   end
 end
+__END__
